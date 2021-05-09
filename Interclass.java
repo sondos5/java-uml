b0869289ea59b0dc86f19392f174881b5b21a1d0
sondos5 <59500975+sondos5@users.noreply.github.com>
2021-05-09 04:44:46 +0200

    Add files via upload

---
+++ Interclass.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author Sondos
+ */
+public class Interclass implements Runnable {
+      public static void main(String[] args) {
+    Interclass obj = new Interclass();
+    Thread thread = new Thread(obj);
+    thread.start();
+    System.out.println("This code is outside of the thread");
+  }
+  public void run() {
+    System.out.println("This code is running in a thread");
+    
+}
+}
---
+++ Myclass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author Sondos
+ */
+public class Myclass  extends Thread {
+  public static void main(String[] args) {
+    Myclass thread = new Myclass();
+    thread.start();
+    System.out.println("This code is outside of the thread");
+  }
+  public void run() {
+    System.out.println("This code is running in a thread");
+  }
+}
+
